afcc19b624d3709ecb5da96833f681274bd33a99
sergeykuptsov84 <skuptsov1984@gmail.com>
2025-01-27 13:14:31 +0300

    Add files via upload

---
+++ Module_7_4.py
@@ -0,0 +1,18 @@
+print("В команде Мастера кода участников: %(team1_num)s!" % {"team1_num": 5})
+print("Итого сегодня в командах участников: %(team1_num)s" " и " "%(team2_num)s!" % {"team1_num": 5, "team2_num": 6})
+
+score_2 = 42
+print("Команда Волшебники данных решила задач: {}!".format(score_2))
+team1_time = 18015.2
+print("Волшебники данных решили задачи за: {} сек!".format(team1_time))
+
+score_1 = 40
+score_2 = 42
+print(f"Команды решили {score_1} и {score_2} задач.")
+
+challenge_result = "победа команды Мастера кода"
+print(f"Результат битвы: {challenge_result} !")
+
+tasks_total = 82
+time_avg =350.4
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
